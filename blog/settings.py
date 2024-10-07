3afe75c24165e7ab549a344f0e1dfe83ec37b545
Aditya Upadhyay <uaditya219@gmail.com>
2024-10-07 22:59:18 +0530

    requirement

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-qk(98ujy)7fv(w#iz7a2+_75dde9wk8^x2!4@^wn01o#lt_56d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iamaditya-blog.onrender.com']
+ALLOWED_HOSTS =['iamaditya-blog.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
