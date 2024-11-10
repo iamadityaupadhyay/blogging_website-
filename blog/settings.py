3e92a7f09e979a1c01cf0a1a448b8cd6fbd02915
Aditya Upadhyay <uaditya219@gmail.com>
2024-11-10 15:04:26 +0530

    new

--- blog/settings.py
+++ blog/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-qk(98ujy)7fv(w#iz7a2+_75dde9wk8^x2!4@^wn01o#lt_56d
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 import os
 
